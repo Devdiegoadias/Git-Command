4718c8684c9777c52844e8f42c6950d55a2b3272
MrTravor <diego.acioly@hotmail.com>
2020-10-14 08:47:42 -0300

    Add project files.

---
+++ GitDemoCommandLine/Controllers/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitDemoCommandLine.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        public bool P1 { get; set; }
+        public DemoController()
+        {
+
+        }
+    }
+}
